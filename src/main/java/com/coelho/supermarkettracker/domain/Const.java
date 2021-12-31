f16b1fd60c2d9cb430af3e89658a8422abf0056b
Rodrigo Coelho Oliveira <coelhoptbr@gmail.com>
2021-12-31 19:14:46 +0100

    Optimizing order screen

---
+++ src/main/java/com/coelho/supermarkettracker/domain/Const.java
@@ -0,0 +1,16 @@
+package com.coelho.supermarkettracker.domain;
+
+public abstract class Const {
+    public static final String COMBOBOX_USERINFO_LABEL = "Select your user";
+    public static final String COMBOBOX_PRODUCT_LABEL = "Select your product";
+    public static final String COMBOBOX_SHOP_LABEL = "Select your shop";
+    public static final String DATEPICKER_ORDER_DATE_LABEL = "Order date";
+    public static final String NUMBERFIELD_PRICE_LABEL = "Price";
+    public static final String CHECKBOX_SPECIAL_OFFER_LABEL = "Special offer";
+    public static final String MSG_NO_HISTORY_LESS_EXPENSIVE = "No history found for less expensive";
+    public static final String MSG_NO_HISTORY_MORE_EXPENSIVE = "No history found for more expensive";
+    public static final String SAVE = "Save";
+    public static final String CLOSE = "Close";
+    public static final String ORDER_NEW = "New order";
+    public static final String ORDER_UPDATE = "Update order";
+}
