df84153dc1332a31e6a5f9a349c826031be98fbd
Rodrigo Oliveira <coelhoptbr@gmail.com>
2022-01-16 19:17:58 +0100

    implementing prefix for documents

--- src/main/java/com/coelho/supermarkettracker/domain/Shop.java
+++ src/main/java/com/coelho/supermarkettracker/domain/Shop.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@Document(collection="shop")
+@Document(collection = "#{T(com.coelho.supermarkettracker.config.CustomMongoCollection).getCollectionNameWithPrefix('shop')}")
 public class Shop {
 
     @NotNull
