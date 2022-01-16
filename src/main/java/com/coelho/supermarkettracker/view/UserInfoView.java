5b34d69728fcd8b7934a9c00cb8e5b4a534ce335
Rodrigo Oliveira <coelhoptbr@gmail.com>
2022-01-16 14:37:18 +0100

    Page titles fix

--- src/main/java/com/coelho/supermarkettracker/view/UserInfoView.java
+++ src/main/java/com/coelho/supermarkettracker/view/UserInfoView.java
@@ -7,11 +7,19 @@ import com.coelho.supermarkettracker.service.UserInfoService;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.HasDynamicTitle;
 import com.vaadin.flow.router.Route;
 import org.vaadin.crudui.crud.impl.GridCrud;
 
 @Route("/userinfo")
-public class UserInfoView extends VerticalLayout {
+public class UserInfoView extends VerticalLayout implements HasDynamicTitle {
+
+    private String title = "Price Tracker - Users";
+
+    @Override
+    public String getPageTitle() {
+        return title;
+    }
 
     public UserInfoView(UserInfoService service) {
         HorizontalLayout hrzToolbar = new HorizontalLayout();
